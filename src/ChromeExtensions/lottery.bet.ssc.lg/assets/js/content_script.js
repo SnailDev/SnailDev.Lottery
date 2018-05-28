86648e68ec5a4dc709bdf57b4dee4b8021d76266
zhaoming <zhaoming@mallcoo.cn>
2018-05-28 22:42:47 +0800

    add 11x5 idea

--- src/ChromeExtensions/lottery.bet.ssc.lg/assets/js/content_script.js
+++ src/ChromeExtensions/lottery.bet.ssc.lg/assets/js/content_script.js
@@ -13,12 +13,19 @@ function injectCustomJs(jsPath) {
     document.head.appendChild(temp);
 }
 
+var timer;
 var lastissue;
 var nextissue;
 var jihua;
-var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var unitArr = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5, 7, 7, 7, 7];
 var step = 0;
+var multiple = 1;
+var maxMoney = 150;
+var buySwitch = false;
+var isSupport = false;
+
+var gameid, gametype;
+
+var unitArr = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5, 7, 7, 7, 7];
 
 function getRandomArrayElements(arr, count) {
     var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
@@ -41,68 +48,242 @@ function isNumber(value) {
 }
 
 function starttimedtask() {
-    setInterval(function () {
+    timer = setInterval(function () {
         var _doc = $(window.frames["mainFrame"].document);
-        if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_1.png') == -1) {
-            console.log('检测到彩种不是重庆时时彩，不做任何操作.');
-            return;
-        }
-
         if (lastissue != _doc.find('#lastissue').text() && isNumber(_doc.find('#lastdigit5').text())) {
             lastissue = _doc.find('#lastissue').text();
             nextissue = _doc.find('#curissue').text();
 
             console.log('当前期数：' + lastissue);
 
-            var kaijiang = [
-                Number(_doc.find('#lastdigit1').text()),
-                Number(_doc.find('#lastdigit2').text()),
-                Number(_doc.find('#lastdigit3').text()),
-                Number(_doc.find('#lastdigit4').text()),
-                Number(_doc.find('#lastdigit5').text())
-            ];
-            console.log('当前开奖号码：' + kaijiang.join(','));
+            var touzhuNum;
+            if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_1.png') > -1) {
+                console.log('彩种：重庆时时彩.');
 
-            if (jihua) {
-                console.log('本期计划为:' + jihua.join(','));
-                var zhong = jihua.filter(v => kaijiang.includes(v));
+                setcqbuy();
+                touzhuNum = docqbusiness(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_12.png') > -1) {
+                console.log('彩种：上海11选5.');
 
-                if (zhong.length == 3) {
-                    console.error('中奖号为：' + zhong.join(',') + '，已中奖');
-                    step = 0;
-                }
-                else {
-                    //console.log('中奖号为：' + zhong.join(',') + '，' + (zhong.length == 3 ? '已中奖' : '未中奖'));
-                    console.log('中奖号为：' + zhong.join(',') + '，未中奖');
-                    step++;
+                setsh11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_10.png') > -1) {
+                console.log('彩种：广东11选5.');
 
-                    if (step > 24) step = 0;
-                }
+                setgd11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_11.png') > -1) {
+                console.log('彩种：山西11选5.');
+
+                setsx11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_13.png') > -1) {
+                console.log('彩种：山东11选5.');
+
+                setsd11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_38.png') > -1) {
+                console.log('彩种：新疆11选5.');
+
+                setxj11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_51.png') > -1) {
+                console.log('彩种：江苏11选5.');
+
+                setjs11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_53.png') > -1) {
+                console.log('彩种：辽宁11选5.');
+
+                setln11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_54.png') > -1) {
+                console.log('彩种：河北11选5.');
+
+                sethb11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else if (_doc.find('#gameLogoImg').attr('src').indexOf('czlogo_65.png') > -1) {
+                console.log('彩种：黑龙江11选5.');
+
+                sethlj11x5buy();
+                touzhuNum = do11x5business(_doc);
+            }
+            else {
+                console.log('该彩种暂时不支持.');
+                // window.clearInterval(timer);
+                return;
             }
 
-            jihua = getRandomArrayElements(nums, 3).sort();
-            console.log(nextissue + '计划：' + jihua.join(','));
-
-            var money = Number($('#walletamount').text());
-            if (money > unitArr[step])
-                autobuy(Number(lastissue) + 1 + '', jihua.join(''), GenerateGuid(), unitArr[step]);
-            else
-                console.log('当前金额小于：' + unitArr[step]);
+            if (buySwitch) {
+                var money = Number($('#walletamount').text());
+                if (money > maxMoney) {
+                    console.log('当前金额大于：' + maxMoney + '，自动下注将自动关闭.');
+                    buySwitch = false;
+                }
+                else if (money > unitArr[step]) {
+                    autobuy(Number(lastissue) + 1 + '', touzhuNum, GenerateGuid(), unitArr[step]);
+                }
+                else {
+                    console.log('当前金额小于：' + unitArr[step] + '，自动下注将自动关闭.');
+                    buySwitch = false;
+                }
+            }
+            else {
+                console.log('自动下注已关闭，不下注');
+            }
         }
     }, 10000);
 }
 
+function setcqbuy() {
+    gameid = 1;
+    gametype = 103;
+}
+
+// 上海
+function setsh11x5buy() {
+    gameid = 12;
+    gametype = 1049;
+}
+
+// 广东
+function setgd11x5buy() {
+    gameid = 10;
+    gametype = 1049;
+}
+
+// 山西
+function setsx11x5buy() {
+    gameid = 11;
+    gametype = 1049;
+}
+
+// 山东
+function setsd11x5buy() {
+    gameid = 13;
+    gametype = 1049;
+}
+
+// 新疆
+function setxj11x5buy() {
+    gameid = 38;
+    gametype = 1049;
+}
+
+// 江苏
+function setjs11x5buy() {
+    gameid = 51;
+    gametype = 1049;
+}
+
+// 辽宁
+function setln11x5buy() {
+    gameid = 53;
+    gametype = 1049;
+}
+
+// 河北
+function sethb11x5buy() {
+    gameid = 54;
+    gametype = 1049;
+}
+
+// 黑龙江
+function sethlj11x5buy() {
+    gameid = 65;
+    gametype = 1049;
+}
+
+function docqbusiness(_doc) {
+    var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    var kaijiang = [
+        Number(_doc.find('#lastdigit1').text()),
+        Number(_doc.find('#lastdigit2').text()),
+        Number(_doc.find('#lastdigit3').text()),
+        Number(_doc.find('#lastdigit4').text()),
+        Number(_doc.find('#lastdigit5').text())
+    ];
+    console.log('当前开奖号码：' + kaijiang.join(','));
+
+    if (jihua) {
+        console.log('本期计划为:' + jihua.join(','));
+        var zhong = jihua.filter(v => kaijiang.includes(v));
+
+        if (zhong.length == 3) {
+            console.error('中奖号为：' + zhong.join(',') + '，已中奖');
+            step = 0;
+        }
+        else {
+            //console.log('中奖号为：' + zhong.join(',') + '，' + (zhong.length == 3 ? '已中奖' : '未中奖'));
+            console.log('中奖号为：' + zhong.join(',') + '，未中奖');
+            step++;
+
+            if (step > 24) step = 0;
+        }
+    }
+
+    jihua = getRandomArrayElements(nums, 3).sort();
+    console.log(nextissue + '计划：' + jihua.join(','));
+
+    return jihua.join('');
+}
+
+function do11x5business(_doc) {
+    var nums = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'];
+
+    var kaijiang = [
+        _doc.find('#lastdigit1').text(),
+        _doc.find('#lastdigit2').text(),
+        _doc.find('#lastdigit3').text(),
+        _doc.find('#lastdigit4').text(),
+        _doc.find('#lastdigit5').text()
+    ];
+    console.log('当前开奖号码：' + kaijiang.join(','));
+
+    if (jihua) {
+        console.log('本期计划为:' + jihua.join(','));
+        var zhong = jihua.filter(v => kaijiang.includes(v));
+
+        if (zhong.length == 5) {
+            console.error('中奖号为：' + zhong.join(',') + '，已中奖');
+            step = 0;
+        }
+        else {
+            //console.log('中奖号为：' + zhong.join(',') + '，' + (zhong.length == 3 ? '已中奖' : '未中奖'));
+            console.log('中奖号为：' + zhong.join(',') + '，未中奖');
+            step++;
+
+            if (step > 24) step = 0;
+        }
+    }
+
+    jihua = getRandomArrayElements(nums, 7).sort();
+    console.log(nextissue + '计划：' + jihua.join(','));
+
+    return jihua.join(' ');
+}
+
 function autobuy(period, number, tgid, unit) {
     var postdata = {
-        "LotteryGameID": 1,
+        "LotteryGameID": gameid,
         "SerialNumber": period,
         "Bets": [{
-            "BetTypeCode": 103,
+            "BetTypeCode": gametype,
             "BetTypeName": "",
             "Number": number,
             "Position": "",
             "Unit": 1,
-            "Multiple": unit,
+            "Multiple": unit * multiple,
             "ReturnRate": 0,
             "IsCompressed": false,
             "NoCommission": false
@@ -164,4 +345,31 @@ document.addEventListener('DOMContentLoaded', function () {
 function sendMessageToBackground(title, message) {
     chrome.runtime.sendMessage({ cmd: 'notify', title: title || '恭喜恭喜', message: message || '笑死小明了' }, function (response) {
     });
+}
+
+function setBuySetings(mult = 1, st = 1, maxmoney = 150) {
+    if (mult > 20) {
+        console.log('设置失败，倍数不能大于20.');
+        return;
+    }
+    if (st < 1) {
+        console.log('设置失败，起始步数不能小于1.');
+        return;
+    }
+
+    multiple = mult;
+    step = st - 1;
+    maxmoney = maxMoney;
+
+    console.log('设置成功，倍数：' + multiple + '，起始步数：' + st + '，金额上限：' + maxMoney);
+}
+
+function startBuy() {
+    buySwitch = true;
+    console.log('success.');
+}
+
+function stopBuy() {
+    buySwitch = false;
+    console.log('success.');
 }
